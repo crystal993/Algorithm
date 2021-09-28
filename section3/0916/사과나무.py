5f13c2eab5c3cee99d3224d84c8580f7b02a5846
Sujeong kim <72599761+crystal993@users.noreply.github.com>
2021-09-28 22:32:16 +0900

    0928 알고리즘 공부
    0928 알고리즘 공부

--- section3/0916/사과나무.py
+++ section3/0916/사과나무.py
@@ -5,7 +5,10 @@ n = int(input())
 data = [list(map(int, input().split())) for _ in range(n)]
 
 sum = 0
+
+# 시작인덱스, 마지막인덱스 전체크기의 절반 이어야 한다.
 sidx = fidx = hlen = int(n/2)
+
 for i in range(n):
     for j in range(sidx, fidx+1):
         sum += data[i][j]
--- section3/0919/곳감(모래시계).py
+++ section3/0919/곳감(모래시계).py
@@ -3,13 +3,41 @@ sys.stdin = open("input.txt","rt")
 
 n = int(input())
 n_data = [list(map(int, input().split())) for _ in range(n)]
-move = int(input())
-m_data = [list(map(int, input().split())) for _ in range(move)]
+rotate = int(input())
+r_data = [list(map(int, input().split())) for _ in range(rotate)]
 
-for i in range(move):
-    for j in range(m_data[2]):
-        if m_data[1] == 0:
-            n_data
+# 1단계 : 회전 실행하는 갯수만큼 for문 돌림.
+for i in range(rotate):
+    for j in range(r_data[i][2]):
+        tmp = 0
+        # 왼쪽
+        if r_data[i][1] == 0:
+           tmp = n_data[r_data[i][0]-1].pop(0)
+           n_data[r_data[i][0]-1].append(tmp)
+           # n_data[r_data[i][0] - 1]
+        # 오른쪽
         else :
+            tmp = n_data[r_data[i][0]-1].pop()
+            n_data[r_data[i][0]-1].insert(0, tmp)
+            # n_data[r_data[i][0] - 1]
+
+# print(n_data)
+sum = 0
+hlen = int(n/2)
+sidx, fidx = 0, n
+for i in range(n):
+    for j in range(sidx, fidx):
+        # print(n_data[i][j] , i, j)
+        sum += n_data[i][j]
+    if i >= hlen:
+        sidx -= 1
+        fidx += 1
+    else:
+        sidx += 1
+        fidx -= 1
+    # print(i,'번째 줄 합:',sum)
+
+
+print(sum)
 
 
