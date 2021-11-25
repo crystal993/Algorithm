6ae9c70f52f4f91a9006241a2284eb8dd3240194
Sujeong kim <72599761+crystal993@users.noreply.github.com>
2021-11-25 21:16:44 +0900

    Create 자릿수의합_함수.py

---
+++ inflearn/section2/AA_0903/자릿수의합_함수.py
@@ -0,0 +1,37 @@
+import sys
+sys.stdin=open("input.txt", "r")
+
+N = int(input())
+num = list(map(int, input().split()))
+
+# print(num)
+
+str_list = []
+sum_list=[]
+def digit_sum(x):
+    sum = 0
+    for num_list in str_list:
+        sum = 0
+        for n in num_list:
+            sum += int(n)
+    sum_list.append(sum)
+
+for n in num:
+    str_list.append(list(str(n)))
+    digit_sum(n)
+
+# print(str_list)
+# print(num)
+# print(sum_list)
+
+max = 0
+midx = 0
+for idx, i in enumerate(sum_list):
+    if i > max:
+        max=i
+        midx=idx
+
+print(num[midx])
+
+
+
