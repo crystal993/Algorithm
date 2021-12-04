1693321d7a58843f6539ec0c9ad0c8d927323496
Sujeong kim <72599761+crystal993@users.noreply.github.com>
2021-12-04 18:30:41 +0900

    Create 계산기.js

---
+++ Javascript/계산기.js
@@ -0,0 +1,9 @@
+function sum(num1, num2) {return num1 + num2; }
+function minus(n1, n2) {return n1-n2;}
+function multiply(n1, n2) {return n1*n2;}
+function division(n1,n2) {return n1/n2;}
+
+console.log(sum(1,5))
+console.log(minus(10,2))
+console.log(multiply(2,3))
+console.log(division(3,5))
